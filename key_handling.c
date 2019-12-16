c9ba0ffc555742676e780619026d8e5d419942a3
Roxana Meixner <roxana.meixner@gmail.com>
2019-12-16 18:41:02 +0100

    updated to use new ubirch c sdk version

--- key_handling.c
+++ key_handling.c
@@ -111,41 +111,33 @@ void create_keys(void) {
     ESP_LOGD(TAG, "publicKey");
     ESP_LOG_BUFFER_HEX_LEVEL(TAG, (const char *) (ed25519_public_key), crypto_sign_PUBLICKEYBYTES, ESP_LOG_DEBUG);
 
-    // create the certificate for the key pair
-    msgpack_sbuffer *sbuf = msgpack_sbuffer_new();
-    // create buffer, protocol and packer
-    ubirch_protocol *proto = ubirch_protocol_new(proto_signed, UBIRCH_PROTOCOL_TYPE_REG,
-                                                 sbuf, msgpack_sbuffer_write, ed25519_sign, UUID);
-    msgpack_packer *pk = msgpack_packer_new(proto, ubirch_protocol_write);
-    // start the ubirch protocol message
-    ubirch_protocol_start(proto, pk);
     // create key registration info
     ubirch_key_info info = {};
     info.algorithm = (char *) (UBIRCH_KEX_ALG_ECC_ED25519);
     info.created = (unsigned int) time(NULL);                           // current time of the system
     memcpy(info.hwDeviceId, UUID, sizeof(UUID));                        // 16 Byte unique hardware device ID
     memcpy(info.pubKey, ed25519_public_key, sizeof(ed25519_public_key));// the public key
-    info.validNotAfter = (unsigned int) (time(NULL) + 31536000);        // time until the key will be valid (now + 1 year)
+    info.validNotAfter = (unsigned int) (time(NULL) +
+                                         31536000);        // time until the key will be valid (now + 1 year)
     info.validNotBefore = (unsigned int) time(NULL);                    // time from when the key will be valid (now)
-    // pack the key registration msgpack
-    msgpack_pack_key_register(pk, &info);
-    // finish the ubirch protocol message
-    ubirch_protocol_finish(proto, pk);
-    // free allocated ressources
-    msgpack_packer_free(pk);
-    ubirch_protocol_free(proto);
+
+    // create protocol context
+    ubirch_protocol *upp = ubirch_protocol_new(UUID, ed25519_sign);
+
+    // create the certificate for the key pair
+    ubirch_protocol_message(upp, proto_signed, UBIRCH_PROTOCOL_TYPE_REG, (const char *) &info, sizeof(info));
 
     // store the generated certificate
-    esp_err_t err = kv_store("key_storage", "certificate", sbuf->data, sbuf->size);
-    if (err != ESP_OK){
+    esp_err_t err = kv_store("key_storage", "certificate", upp->data, upp->size);
+    if (err != ESP_OK) {
         ESP_LOGW(TAG, "key certificate could not be stored in flash");
     }
     // store the keys
-    if (store_keys() != ESP_OK){
+    if (store_keys() != ESP_OK) {
         ESP_LOGW(TAG, "generated keys could not be stored in flash");
     }
-    // free the allocated resources
-    msgpack_sbuffer_free(sbuf);
+    // free allocated resources
+    ubirch_protocol_free(upp);
 }
 
 
